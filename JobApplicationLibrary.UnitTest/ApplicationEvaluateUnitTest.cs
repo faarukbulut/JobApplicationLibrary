7f18e9a5a159dfcdb1bb66774a1b957ebcb56e99
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-04-24 03:23:12 +0300

    unit test projesi eklendi.

---
+++ JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace JobApplicationLibrary.UnitTest
+{
+    public class ApplicationEvaluateUnitTest
+    {
+        [Test]
+        public void Test1()
+        {
+            //
+        }
+    }
+}
